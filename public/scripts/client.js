5ef42c384dc57840460ea56a415646676c4700d6
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-01-31 12:32:18 -0600

    got ng-model and ng-repeat to work and display on DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,20 @@
-var app = angular.module('employeeApp', []);
+var app = angular.module('employerApp', []);
 
-app.controller('EmployeeController', function (){
+app.controller('EmployerController', function (){
   //all the logic we want the controller to do is in this function
   //similar to document ready
-  console.log('EmployeeController loaded');
+  console.log('EmployerController loaded');
   //$scope is the alternative
-  
+
   //controller as syntax
-  var intro = this;
+  var employer = this;
+
+  employer.employees = [];
+
+  employer.addEmployee = function (){
+    employer.employees.push(angular.copy(employer.employee));
+    console.log(employer.employees);
+  };
+
+
+});
