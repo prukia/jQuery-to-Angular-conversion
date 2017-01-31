df33d39ef56e77d905d399b33905e4333afb5539
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-01-31 13:51:27 -0600

    monthly expenditure working via angular

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,10 +10,20 @@ app.controller('EmployerController', function (){
   var employer = this;
 
   employer.employees = [];
+  //set object to a global entity
+  employer.totalMonthlySalary = 0;
 
   employer.addEmployee = function (){
     employer.employees.push(angular.copy(employer.employee));
     console.log(employer.employees);
+
+    var totalMonthlySalary = employer.totalMonthlySalary;
+    totalMonthlySalary = Number(totalMonthlySalary);
+    totalMonthlySalary += (employer.employee.salary/12);
+    //set controller name to change to totalMonthlySalary
+    employer.totalMonthlySalary = totalMonthlySalary;
+    // toLocaleString("en-US", {style: 'currency', currency: 'USD'});
+    //employer.salary = totalMonthlySalary;
   };
 
 
